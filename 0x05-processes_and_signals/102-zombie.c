ff2510ba5e8e81ab2bd543ed92aca2e2c56a5c66
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-04 10:00:49 +0000

    Zombie mode

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
-#include <sys.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-/**
- * 102-zombie - C function that creates 5 zombie processes
- *
- * Description:
- * For every zombie process created, it displays Zombie process created, PID: ZOMBIE_PID
- * Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
- * When your code is done creating the parent process and the zombies, use the function infinite_while
- *
- * Returns: ...
- */
 
 int infinite_while(void);
 
-int i;
-int main()
+/**
+ * main - C function that creates 5 zombie processes
+ *
+ * Description:
+ * For every zombie process created, it displays Zombie process created,
+ * PID: ZOMBIE_PID
+ * Your code should use the Betty style. It will be checked using
+ * betty-style.pl and betty-doc.pl
+ * When your code is done creating the parent process and the zombies,
+ * use the function infinite_while
+ *
+ * Return: void
+ */
+
+int main(void)
 {
-        for (i = 0; i < 5; i++)
-        {
-                pid_t child_pid;
+	int i;
 
-                child_pid = fork();
-                if (child_pid == 0)
-                {
-                        printf("Zombie process created, PID: %d\n", getpid());
-                        return (0);
+	for (i = 0; i < 5; ++i)
+	{
+		pid_t child_pid;
 
-                }
-        }
-        infinite_while();
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+		}
+	}
+	infinite_while();
+	return (0);
 }
+
+/**
+ * infinite_while - infinite while loop that sleeps for 1 min indefinitely
+ *
+ * Return: void
+ */
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
